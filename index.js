145b6879af474da35dae4954ac97fe3f38e7f684
SheIsAddiee <enobongadegoke2007@gmail.com>
2025-08-13 14:42:23 +0100

    Second Push

--- index.js
+++ index.js
@@ -4,15 +4,46 @@
 //const numberToWords = require("amount-to-words");
 //const chequeconvert= require("chequeconvert");
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const PORT = 3000;
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',       // change if needed
+    password: '',       // your DB password
+    database: 'mini_market'  // your database name
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('❌ Database connection failed:', err);
+        return;
+    }
+    console.log('✅ Connected to MySQL database');
+});
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
 // Root route
 app.get('/', (req, res) => {
     res.send('Hello World from Node.js API!');
+});
+
+app.get('/products', (req, res) => {
+    db.query('SELECT * FROM products', (err, results) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Database query failed' });
+        } else {
+            res.send({
+                responseCode:"00",
+                responseMessage:"Successfull",
+                data:results
+            })
+            //res.json(results);
+        }
+    });
 });
 
 app.post("/converter",(req,res)=>{
